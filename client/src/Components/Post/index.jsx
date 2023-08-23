5942f4f53e22fb2b78d09546a51cf81c80e39c04
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-08-23 19:53:48 +0300

    CLIENT <> Implemented Like procedure

--- client/src/Components/Post/index.jsx
+++ client/src/Components/Post/index.jsx
@@ -1,43 +1,41 @@
-import "../Post/style.css"
-import non from "../../assets/heart.svg"
-import love from "../../assets/red.svg"
 import React, { useState } from 'react';
-const Post = ({first_name,last_name,author,description,likes,image_url}) =>{
-  
+import non from "../../assets/heart.svg";
+import love from "../../assets/red.svg";
+import "../Post/style.css";
+
+const Post = ({ first_name, last_name, author, description, likes, image_url, onLike }) => {
+
     let [image, setImage] = useState(non);
-    let[likescount,setLikesCount] = useState(0)
 
     const handleToggle = () => {
-      if (image==non) {
-       setImage(love);
-       setLikesCount(likescount=likescount+1)
-    } else {
-      setImage(non);
-      setLikesCount(likescount=likescount-1)
-    }
-  };
-    return(   <>
-        <div className="post-container">
-            <div className="post-head">
-               <h3>{first_name} {last_name}</h3>
+        if (image === non) {
+            setImage(love);
+            onLike(); // Call the onLike callback to increase likes
+        } else {
+            setImage(non);
+        }
+    };
+
+    return (
+        <>
+            <div className="post-container">
+                <div className="post-head">
+                    <h3>{first_name} {last_name}</h3>
+                </div>
+                <div className="post-body">
+                    <img src={image_url} alt="" className="test" />
+                </div>
+                <div className="post-foot" onClick={handleToggle}>
+                    <img src={image} alt="" />
+                </div>
+                <span className="likes">{likes} likes</span>
+                <div className="post-foot-desc">
+                    <span className="author">{author}</span>
+                    <span>{description}</span>
+                </div>
             </div>
-            <div className="post-body">
-            <img src= {image_url} alt="" className="test"/>
-            </div>
-            <div className="post-foot">
-                <img src={non} alt="" onClick={handleToggle}/>
-            </div>
-            <span className="likes">{likes} likes</span>
-            <div className="post-foot-desc">
-              <span className="author">{author}</span>
-              <span>{description}</span>
-            </div>
-            
-           
-        </div>
-        
         </>
     )
- 
 }
-export default Post
+
+export default Post;
--- client/src/Components/Posts/index.jsx
+++ client/src/Components/Posts/index.jsx
@@ -3,20 +3,29 @@ import Post from '../Post';
 import "../Posts/style.css"
 
 
-const Posts = ({books}) => {
+const Posts = ({ books }) => {
+    const [booksState, setBooksState] = useState(books);
+
+    const handleLike = (bookIndex) => {
+        const updatedBooks = [...booksState];
+        updatedBooks[bookIndex].likes += 1;
+        setBooksState(updatedBooks);
+    }
+
     return (
         <>
-             {books.map((book, index) => (
+            {books.map((book, index) => (
                 <Post
                     key={index}
                     first_name={book.first_name}
                     last_name={book.last_name}
                     author={book.author}
                     description={book.description}
-                    image_url={`data:image/jpeg;base64,${book.image_url}`} 
+                    image_url={`data:image/jpeg;base64,${book.image_url}`}
                     likes={book.likes}
+                    onLike={() => handleLike(index)}
                 />
-            ))}  
+            ))}
         </>
     );
 };
